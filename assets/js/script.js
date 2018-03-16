bb94b6c112defdf5318fd910549bb787edddc6d0
rran14 <rranjan14@sapient.com>
2018-03-16 20:24:08 +0530

    Thanks page setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	$('.inner-contant').on('click','.download_btn', function(){
 		var queryString = location.search.split('=');
 		if(queryString[1] && validateEmail(queryString[1])){
-			window.location.href = "/thanks.html?" + queryString[1];
+			window.location.href = "thanks.html?" + queryString[1];
 		}else{
 			$('.error').css('display','block');
 		}
