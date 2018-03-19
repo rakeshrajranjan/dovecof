ecf690e427020a09843d160f63c8af4ebe0cb02c
rran14 <rranjan14@sapient.com>
2018-03-19 13:09:16 +0530

    Recaptcha

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,16 @@ var validateEmail = function(email){
 	var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 	return emailReg.test(email);
 }
+var onloadCallback = function(){
+	grecaptcha.render("gid",{
+		'sitekey':'6LfVHAETAAAAAK5e91YdgFCgsVEjtVkQfPrbNxKS'
+	});
+}
 $(document).ready(function(){
 	$('.inner-contant').on('click','.download_btn', function(){
 		var queryString = location.search.split('=');
 		if(queryString[1] && validateEmail(queryString[1])){
-			window.location.href = "thanks.html?" + queryString[1];
+			window.location.href = "thankyou.html?" + queryString[1];
 		}else{
 			$('.error').css('display','block');
 		}
