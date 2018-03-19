4944b039924da06d3b3bc1214d4b46c08903362d
rran14 <rranjan14@sapient.com>
2018-03-19 16:48:28 +0530

    Coupon code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,53 @@ var validateEmail = function(email){
 	return emailReg.test(email);
 }
 var onloadCallback = function(){
-	grecaptcha.render("gid",{
+	grecaptcha.render("welcome_recaptcha",{
 		'sitekey':'6LfVHAETAAAAAK5e91YdgFCgsVEjtVkQfPrbNxKS'
 	});
 }
-$(document).ready(function(){
-	$('.inner-contant').on('click','.download_btn', function(){
-		var queryString = location.search.split('=');
-		if(queryString[1] && validateEmail(queryString[1])){
-			window.location.href = "thankyou.html?" + queryString[1];
-		}else{
-			$('.error').css('display','block');
+var printCoupon = function(){
+	var webPin = window.btoa($('#welcome_email').val());
+	if(webPin.length > 50) webPin = webPin.substring(0,50);
+	$("#welcome_form").append('<input type="hidden" name="webpin" value='+webPin+' />');
+
+	var selectionCode= $('.download_btn').attr('data-selection-offercode');
+	var brickEncryptionUrl = "https://campaignwidgetservices-uat.unileversolutions.com/dove_us_dakota_en_us/en-us/campaign/getcoupon/"+selectionCode+"/"+webPin;
+	$.ajax({
+		  url:brickEncryptionUrl,
+		  type: "get", //send it through get method
+		  
+		  success: function(response) {
+			//alert("CPT = "+response.CPT + " Check Code = " +response.CheckCode);
+			window.location.href="http://bricks.coupons.com/enable.asp?o="+selectionCode+"&c="+response.CheckCode + "&p="+webPin+"&cpt=" + response.CPT;
+		},
+		error: function(xhr) {
+		//Do Something to handle error
 		}
 	});
+};
+$(document).ready(function(){
+	$('.inner-contant').on('click','.download_btn', function(){
+		var email = $('#welcome_email').val();
+		if(email!="" && validateEmail(email) ){	
+			$('.email_error').css('display','none');		
+			var response = grecaptcha.getResponse();
+			if (response.length == 0) {
+				var destElementOffset =  window.innerHeight - $('.g-recaptcha').position().top;
+				$('html, body').animate({ scrollTop: destElementOffset }, 100);
+				$('.recaptcha_error').show();
+				return false;
+			}
+			else {
+				$('.recaptcha_error').hide();
+				printCoupon();
+				return false;
+			}
+		}else{
+			$('#welcome_email').focus();
+			$('.email_error').css('display','block');
+			return false;
+		}
+	});
+	
+	
 });
